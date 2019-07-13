8234274c3d4bf27b6b10f79270ad04a936b3dafd
leeduck <leeduck13@naver.com>
2019-07-14 01:36:31 +0900

    read 2 data(crime, homeless)
    # Conflicts:
    #	yeah.py

---
+++ projectUSC/Homeless.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data_homeless = pd.read_csv('../../LA_Homelessness_Count.csv')
+data_homeless.head()
--- projectUSC/main.py
+++ projectUSC/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 data_crime = pd.read_csv('../../Crime_Data_from_2010_to_Present.csv')
 data_crime.head()
+print(data_crime)
 
 data_homeless = pd.read_csv('../../LA_Homelessness_Count.csv')
 data_homeless.head()
