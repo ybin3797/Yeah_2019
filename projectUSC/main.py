8b3afd059febbe1c7738820606fae09dc7a51164
Yebin <u3797@naver.com>
2019-07-12 23:08:04 -0700

    yebin_test

--- projectUSC/main.py
+++ projectUSC/main.py
@@ -1,3 +1,6 @@
 import pandas as pd
-data = pd.read_csv('')
-data.head()
+data_crime = pd.read_csv('../../Crime_Data_from_2010_to_Present.csv')
+data_crime.head()
+
+data_homeless = pd.read_csv('../../LA_Homelessness_Count.csv')
+data_homeless.head()
